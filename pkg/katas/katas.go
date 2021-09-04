8465cd91d4cd6093417884fd859999d461bca8b3
Josue Silva <josuesilva@7graus.com>
2021-09-04 12:46:20 +0100

    fix(ApplySRP): Apply Single Responsability Principle in the Project
    To improve the manutenability and legibility and follow the best practices, we separate the implementations in segregated files.

--- pkg/katas/katas.go
+++ pkg/katas/katas.go
@@ -1,52 +1,18 @@
 package katas
 
+import (
+	"github.com/jmsilvadev/cycloid/katas/internal/iterable"
+	"github.com/jmsilvadev/cycloid/katas/internal/recursive"
+)
+
 type IKata02 interface {
 	chop(subject int, listValues []int) int
 }
 
-type IterableKata struct{}
-
-type RecursiveKata struct{}
-
-func NewIterable() *IterableKata {
-	return &IterableKata{}
+func NewIterable() *iterable.IterableKata {
+	return &iterable.IterableKata{}
 }
 
-func NewRecursive() *RecursiveKata {
-	return &RecursiveKata{}
-}
-
-func (k *IterableKata) chop(subject int, listValues []int) int {
-	stPos := 0
-	endPos := len(listValues) - 1
-	for stPos <= endPos {
-		midPos := (stPos + endPos) / 2
-		if listValues[midPos] == subject {
-			return midPos
-		}
-		if subject > listValues[midPos] {
-			stPos = midPos + 1
-			continue
-		}
-		endPos = midPos - 1
-	}
-	return -1
-}
-
-func (k *RecursiveKata) chop(subject int, listValues []int) int {
-	return k.chopRecursive(subject, listValues, 0, len(listValues)-1)
-}
-
-func (k *RecursiveKata) chopRecursive(subject int, listValues []int, stPos int, endPos int) int {
-	if stPos > endPos {
-		return -1
-	}
-	midPos := (stPos + endPos) / 2
-	if listValues[midPos] == subject {
-		return midPos
-	}
-	if subject > listValues[midPos] {
-		return k.chopRecursive(subject, listValues, midPos+1, endPos)
-	}
-	return k.chopRecursive(subject, listValues, stPos, midPos-1)
+func NewRecursive() *recursive.RecursiveKata {
+	return &recursive.RecursiveKata{}
 }
